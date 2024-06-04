f5fc18d85883cf2b7fffc917809c8de14447be9f
alinahyder <alina@granitewood.com>
2024-06-03 23:36:50 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        double itemPrice= 3.50;
+        double salesTax = .05;
+        double itemTax;
+
+        itemTax= itemPrice*salesTax;
+        System.out.println("The sales tax is "+itemTax);
+
+
+    }
+}
